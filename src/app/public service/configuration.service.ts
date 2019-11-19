b0c337b27f65cd3aa10c754978feda069f5fd6f7
sachin <sachin@stackmint.com>
2019-11-19 20:21:03 +0530

    Close document functionality added

--- src/app/public service/configuration.service.ts
+++ src/app/public service/configuration.service.ts
@@ -9,7 +9,7 @@ export class ConfigurationService {
 
   // public static baseUrl = `http://13.127.6.128:8085/`;  //server side
 
-  public static baseUrl = `http://192.168.0.115:4001/`;  //local-Ajay
+  public static baseUrl = `http://192.168.0.115:7001/`;  //local-Ajay
 
   // public static baseUrl = `http://192.168.0.166:8082/`;  //local-Harshad
 
